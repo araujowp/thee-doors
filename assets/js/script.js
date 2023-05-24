bb1534ce33fc92586b5f22d3de1e403553e47a9f
Wagner Araujo <araujowp@gmail.com>
2023-05-24 17:22:09 -0300

    added restart button

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,20 +11,20 @@ firstDoor = 0;
 secondDoor = 0;
 thirdDoor = 0;
 
+message2.innerText = sorted;
+
 function doorClick(door){
     
     selectedDoor = door.innerText;
 
-    alert('sorteada ' + sorted + ' segunda ' + secondDoor + ' selecionada ' + selectedDoor);
-    
-    if (firstDoor == 0){
+    if (firstDoor === 0){
         door.classList.add('door_marked');
         firstDoor = selectedDoor;
         message.textContent = 'Open other door';
         return;
     }
     
-    if(firstDoor === selectedDoor){
+    if(firstDoor === selectedDoor && secondDoor === 0 ){
         door.classList.remove('door_marked');
         firstDoor = 0;
         message.textContent = 'Choose a door';
@@ -32,7 +32,6 @@ function doorClick(door){
     }
     
     if(secondDoor === 0){
-        alert('entrou na segunda ');
         secondDoor = selectedDoor;
         
         if(selectedDoor == sorted){
@@ -56,4 +55,16 @@ function doorClick(door){
 function loose(door){
     door.classList.add('door_wrong');
     message.textContent = 'Lose wrong door';
+}
+
+function restartClick(myButton){
+    firstDoor = 0;
+    secondDoor = 0;
+    thirdDoor = 0;
+    door1.classList.remove(...door1.classList);
+    door1.classList.add('door','door_face');
+    door2.classList.remove(...door2.classList);
+    door2.classList.add('door','door_face');
+    door3.classList.remove(...door3.classList);
+    door3.classList.add('door','door_face');
 }
